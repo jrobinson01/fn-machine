9d26652ba3406649c3dfcb1c867262b70eea371c
John Robinson <john.robinson@banno.com>
2019-09-07 19:34:59 -0400

    only run enter, exit and callback if a transition was run

--- src/fn-machine.js
+++ src/fn-machine.js
@@ -26,19 +26,23 @@ export default function machine(states, initialState, initialContext, changeCb =
     if (active) {
       const transitionKey = Object.keys(active.transitions || {}).find(k => k === event);
       const transition = active.transitions[transitionKey];
-      // if there's a transition, call it, otherwise just return the current state.
+      // if there's a transition, call it, otherwise just keep the current state.
       const next = transition ? transition(detail, context) : {state: current, context};
-      // if the current state has an exit function, run it.
-      active.exit && active.exit();
-      const newState = states.find(s => s.name === next.state);
-      // if the new state has an enter function, run it as well.
-      newState.enter && newState.enter(next.context || context);
-      // update current
-      current = next.state;
-      // update next.context if necessary
-      next.context = context = next.context ? next.context : context;
-      // call callback with the latest state.
-      changeCb(next);
+      // we only want to run exit, enter and the callback IF a transition was run.
+      if (transition) {
+        // if the current state has an exit function, run it.
+        active.exit && active.exit();
+        const newState = states.find(s => s.name === next.state);
+        // if the new state has an enter function, run it as well.
+        newState.enter && newState.enter(next.context || context);
+        // update current
+        current = next.state;
+        // update next.context if necessary
+        next.context = context = next.context ? next.context : context;
+        // call callback with the latest state.
+        changeCb(next);
+      }
+
       return next;
     }
     return currentState;
